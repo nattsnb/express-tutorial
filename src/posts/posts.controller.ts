14b370317507570e8779d1804b13755f604f078c
Natasza ASzatan <natiye.nb@gmai.com>
2025-02-24 22:31:31 +0100

    updated post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,6 +5,7 @@ import postModel from './posts.model';
 import PostNotFoundException from "../exceptions/PostNotFoundException";
 import validationMiddleware from "../middleware/validation.middleware";
 import CreatePostDto from "./post.dto";
+import authMiddleware from "../middleware/auth.middleware";
 
 class PostsController implements Controller {
   public path = '/posts';
@@ -18,9 +19,11 @@ class PostsController implements Controller {
   private initializeRoutes() {
     this.router.get(this.path, this.getAllPosts);
     this.router.get(`${this.path}/:id`, this.getPostById);
-    this.router.delete(`${this.path}/:id`, this.deletePost);
-    this.router.patch(`${this.path}/:id`, validationMiddleware(CreatePostDto, true), this.modifyPost);
-    this.router.post(this.path, validationMiddleware(CreatePostDto), this.createPost);
+    this.router
+      .all(`${this.path}/*`, authMiddleware)
+      .patch(`${this.path}/:id`, validationMiddleware(CreatePostDto, true), this.modifyPost)
+      .delete(`${this.path}/:id`, this.deletePost)
+      .post(this.path, authMiddleware, validationMiddleware(CreatePostDto), this.createPost);
   }
 
   private getAllPosts = (request: express.Request, response: express.Response) => {
