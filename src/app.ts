34d24cb768abf476f435cc6219152acdfcde3b3d
Natasza ASzatan <natiye.nb@gmai.com>
2025-02-28 12:04:38 +0100

    changed Access-Control-Allow-Origin to match new frontend

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(cookieParser());
     this.app.use(function (req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
       next();
     });
